375b73f1d1487651c6cddae9b6dab33bb306f68c
Puc199 <pucci.1994172@studenti.uniroma1.it>
2026-05-18 14:14:46 +0200

    a

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+/*  document.addEventListener('DOMContentLoaded', () => {
     const swiper = new Swiper('.swiper-container', {
         loop: true,
         spaceBetween: 30,
